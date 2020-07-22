d2b533101a4f238b677e6cc8dc6294760ac7785f
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-22 21:31:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition, 
   attr_reader :brand
   
-  BRANDS = []
+  # BRANDS = []
   
   def initialize(brand)
     @brand = brand
@@ -15,7 +15,7 @@ class Shoe
   end
 
   # def brand= (brand)
-  #   @brand = brand
+  #   @brand = brands
   #   BRANDS << brand
   # end
     
