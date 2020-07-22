32166bfa14d71597997ba2d74b35109e23b04297
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-22 21:52:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,10 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-   if BRANDS.include? brand
+   if BRANDS.include! brand
     BRANDS << brand
    end
+ else BRANDS
   end
 
   def cobble
